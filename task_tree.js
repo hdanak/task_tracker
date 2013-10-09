b5cbfc611419fba2155681182aa289add565c583
Hike Danakian <hike@hdanak.com>
2013-10-08 18:32:32 -0700

    Add task check boxes.

--- task_tree.js
+++ task_tree.js
@@ -1,16 +1,10 @@
-$('.task-panel').on('click', '.closed,.open', function(evt) {
-  if ($(this).is(evt.target) || $(this).find('> span').is(evt.target)) {
-    $(this).siblings('li.open').toggleClass('open closed');
-    $(this).toggleClass('open closed');
-  }
-  return false;
-}).on('submit', '.new.activity > form', function(evt) {
+$('.task-panel').on('submit', '.new.activity > form', function(evt) {
   $(this).parent().before('<li class="closed activity">'
       + '<span class="button-panel">'
         + '<button class="icon-remove"></button>'
         + '<button class="icon-pencil"></button>'
       + '</span>'
-      + '<span>' + $(this).find('> input').val() + '</span>'
+      + '<span class="label">' + $(this).find('> input').val() + '</span>'
       + '<ul class="task-list">'
         + '<li class="new task">'
           + '<form><input type="text"></input></form>'
@@ -19,18 +13,28 @@ $('.task-panel').on('click', '.closed,.open', function(evt) {
     + '</li>');
   $(this).find('> input').val('')
   return false;
+}).on('click', '.closed,.open', function(evt) {
+  if ($(this).is(evt.target) || $(this).find('> .label').is(evt.target)) {
+    $(this).siblings('li.open').toggleClass('open closed');
+    $(this).toggleClass('open closed');
+  }
+  return false;
 }).on('click', '.activity > .button-panel .icon-remove', function(evt) {
   $(this).parents('li').first().remove();
 }).on('submit', '.new.task > form', function(evt) {
-  $(this).parent().before('<li class="task">'
+  $(this).parent().before('<li class="unchecked task">'
       + '<span class="button-panel">'
         + '<button class="icon-remove"></button>'
         + '<button class="icon-pencil"></button>'
       + '</span>'
-      + '<span>' + $(this).find('> input').val() + '</span>'
-      + '<ul class="task-list"></ul>'
+      + '<span class="label">' + $(this).find('> input').val() + '</span>'
     + '</li>');
   $(this).find('> input').val('')
+  return false;
+}).on('click', '.checked,.unchecked', function(evt) {
+  if ($(this).is(evt.target) || $(this).find('> .label').is(evt.target)) {
+    $(this).toggleClass('checked unchecked');
+  }
   return false;
 }).on('click', '.task > .button-panel .icon-remove', function(evt) {
   $(this).parents('li').first().remove();
